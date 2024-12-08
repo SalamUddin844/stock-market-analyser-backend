b1dc36c28e7c786384d3639d780c65a455d86239
SalamUddin844 <mdslamuddin844@gmail.com>
2024-12-09 01:06:52 +0600

    updatedd host

--- api/main.py
+++ api/main.py
@@ -171,4 +171,6 @@ def download_file(filename):
 def index():
     return render_template('index.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
